ee87a47b83687f3c9f82f3945c9b89e033b328e8
Nihiro Tamura <tamura.nihiro@irl.sys.es.osaka-u.ac.jp>
2025-01-21 16:22:43 +0900

    目標値&パラメータsubscribe実装

--- PID_ROS2/PID_ROS2.ino
+++ PID_ROS2/PID_ROS2.ino
@@ -7,11 +7,20 @@
 #define PUB_PERIOD_MS 10
 
 //------Topic names--------------------------------------------------------------------
-#define SUB_TOPICNAME "/board1/aout"
-#define PUB_TOPICNAME "/board1/ain"
+#define SUB_TOPICNAME "/board1/sub"
+#define PUB_TOPICNAME "/board1/pub"
 
 //------LEDの定義ピン--------------------------------------------------------------------
 #define LED 13
+
+//------目標値の個数--------------------------------------------------------------------
+#define POT_DESIRED 6
+
+//------パラメータの個数--------------------------------------------------------------------
+#define PARAMETER 0
+
+//------Subscribeするデータの個数--------------------------------------------------------------------
+#define SUBSCRIBE 6 + PARAMETER //  POT_DESIRED + PARAMETER
 
 //------AD/DA conv channels--------------------------------------------------------------------
 //　アナログ入力のピン数
@@ -25,7 +34,7 @@
 //const int ain_channels[ANALOG_IN_CH] = {14,15,16,17,18,19,20,21,22,23,24,25,26,27,38,39,40,41};
 const int ain_channels[ANALOG_IN_CH] = {26,27,38,39,40,41};
 
-// PWMピンの定義
+//  PWMピンの定義
 const int aout_channels[ANALOG_OUT_CH] = {0,1,2,3,4,5,6,7,8,9,28,29};
 
 //=============================================================================================================================================
@@ -48,17 +57,17 @@ const int aout_channels[ANALOG_OUT_CH] = {0,1,2,3,4,5,6,7,8,9,28,29};
 #include <rclc/rclc.h>
 #include <rclc/executor.h>
 
-//　使用するメッセージの型のライブラリ
+//  使用するメッセージの型のライブラリ
 #include <std_msgs/msg/u_int16_multi_array.h>
 #include <std_msgs/msg/multi_array_dimension.h>
 #include <std_msgs/msg/multi_array_layout.h>
 
 //------マクロの定義--------------------------------------------------------------------
 #define RCCHECK(fn) { rcl_ret_t temp_rc = fn; if((temp_rc != RCL_RET_OK)){error_loop();}}
-// ROS2の関数fnが成功したかどうかを確認。成功したら何もせず次の処理に進み、失敗したらerror_loop()を呼び出しエラー状態にする
+//  ROS2の関数fnが成功したかどうかを確認。成功したら何もせず次の処理に進み、失敗したらerror_loop()を呼び出しエラー状態にする
 
 #define RCSOFTCHECK(fn) { rcl_ret_t temp_rc = fn; if((temp_rc != RCL_RET_OK)){}}
-// ROS2の関数fnが成功したかどうかを確認。成功したら何もせず次の処理に進み、失敗しても何もせずスルーする
+//  ROS2の関数fnが成功したかどうかを確認。成功したら何もせず次の処理に進み、失敗しても何もせずスルーする
 
 //------オブジェクトの定義--------------------------------------------------------------------
 //  使用するROS2メッセージの型
@@ -77,30 +86,33 @@ rcl_timer_t timer;
 //=============================================================================================================================================
 
 //------グローバル変数の定義--------------------------------------------------------------------
-//　アナログ出力の配列
-volatile uint16_t aout[12];
+//　subscribeメッセージの配列
+volatile uint16_t sub[SUBSCRIBE];
 
-//　アナログ入力の配列
-volatile uint16_t ain[6];
+//　publishメッセージの配列
+volatile uint16_t pub[6];
 
 //  POT値
 volatile uint16_t POT_realized[6] = {0, 0, 0, 0, 0, 0};
 
+//  目標値の初期値{腕の閉190-394開, 腕の下287-534上, 上腕の旋回内87-500外, 肘の伸124-635曲, 前腕の旋回内98-900外, 小指側縮48-822伸}
+volatile uint16_t POT_desired[6] = {
+  350, 548, 113, 220, 130, 500
+};
+
+//  parameter値
+//volatile uint16_t parameter[PARAMETER] = {0};
+
 //---PID制御--------------------------------------------------------------------
 //  PIDゲイン
 const float kp[6] = {
-  0.7, 2.1, 0.85, 0.2, 1.6, 0.6
+  1.0, 0.0, 0.0, 0.0, 0.0, 0.0
 };
 const float ki[6] = {
   0.0, 0.0, 0.0, 0.0, 0.0, 0.0
 };
 const float kd[6] = {
-  20.0, 25.0, 20.0, 2.0, 0.0, 0.0
-};
-
-//  目標値{腕の閉190-394開, 腕の下287-534上, 上腕の旋回内87-500外, 肘の伸124-635曲, 前腕の旋回内98-900外, 小指側縮48-822伸}
-volatile uint16_t POT_desired[6] = {
-  390, 548, 113, 220, 130, 500
+  0.0, 0.0, 0.0, 0.0, 0.0, 0.0
 };
 
 //  各自由度ごとの圧力の正方向とポテンショメータの正方向の対応を整理
@@ -135,18 +147,18 @@ int VEAB_desired[12] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
 //---RCフィルタ--------------------------------------------------------------------
 //  ローパスフィルタの係数　係数a=1/(2*pi*fc*dt + 1)   fc[Hz]:カットオフ周波数、dt[s]:サンプリング周期
 //  カットオフ周波数:Hz, サンプリング周期:で設定
-const float coef_lpf_veab = 0.75; //VEAB
-const float coef_lpf_pot = 0.3;   //POT
+const float coef_lpf_veab = 0.75; //  VEAB
+const float coef_lpf_pot = 0.3;   //  POT
 
 //  ローパスフィルタの値保持変数
-int veab_filter[12] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}; //VEAB
+int veab_filter[12] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}; //  VEAB
 
 //  ローパスフィルタ用前回の値保持変数
-int previous_value_veab[12] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}; //VEAB
+int previous_value_veab[12] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}; //  VEAB
 
 
 //  ローパスフィルタ用初回判定
-int initial_lpf_veab[12] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}; //VEAB
+int initial_lpf_veab[12] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};  // VEAB
 
 //---移動平均法--------------------------------------------------------------------
 //  ローパスフィルタ適用POT値格納構造体
@@ -160,20 +172,23 @@ struct Result_LPF {
 };
 
 //  標本数
-#define LPF_kosuu 50
+#define LPF_KOSUU 18
 
 //  使用するチャンネル数
 #define NUM_CHANNELS 6
 
 //  CircularBufferを使用してリングバッファを作成
-CircularBuffer<int, LPF_kosuu> buffers[NUM_CHANNELS]; // 各チャンネル用のリングバッファ
+CircularBuffer<int, LPF_KOSUU> buffers[NUM_CHANNELS]; //  各チャンネル用のリングバッファ
 
-//移動平均法での合計値と平均値格納
-long pot_sum[NUM_CHANNELS] = {0}; // 各チャンネルの値の合計
-int POT[NUM_CHANNELS] = {0};     // 各チャンネルの移動平均値
+//  移動平均法での合計値と平均値格納
+long pot_sum[NUM_CHANNELS] = {0}; //  各チャンネルの値の合計
+int POT[NUM_CHANNELS] = {0};      //  各チャンネルの移動平均値
 
 //------スレッド間で共有リソースへのアクセスを制御するための排他制御（mutex: ミューテックス）を定義--------------------------------------------------------------------
 Threads::Mutex adc_lock;
+
+//------subscribeの初回判定--------------------------------------------------------------------
+int sub_count = 0;
 
 //=============================================================================================================================================
 
@@ -181,7 +196,7 @@ Threads::Mutex adc_lock;
 //　エラー関数
 void error_loop(){
   while(1){
-    //　エラー状態のときLED点滅
+    //  エラー状態のときLED点滅
     digitalWrite(LED, !digitalRead(LED));
     delay(100);
   }
@@ -210,9 +225,18 @@ void thread_callback() {
       POT_realized[4] = pot.POT4;
       POT_realized[5] = pot.POT5;
 
-      //  ROS用の配列にPOT値を格納
+      //  publishメッセージの配列にPOT値を格納
       for(int i = 0; i < ANALOG_IN_CH; i++){
-        ain[i] = POT_realized[i];
+        pub[i] = POT_realized[i];
+      }
+
+      //  subscribeしたメッセージを目標値に格納
+      for(int i = 0; i < POT_DESIRED; i++){
+        if(sub_count == 0){
+          sub[i] = POT_desired[i];
+        }
+
+        POT_desired[i] = sub[i];
       }
 
     }
@@ -223,15 +247,15 @@ void thread_callback() {
     //  RCローパスフィルタ適用(VEAB)
     for(int i = 0; i < 12; i++){
 
-      //ローパスフィルタ関数呼び出し
+      //  ローパスフィルタ関数呼び出し
       veab_filter[i] = RC_LPF(VEAB_desired[i], previous_value_veab[i], initial_lpf_veab[i], coef_lpf_veab);
 
       initial_lpf_veab[i] += 1;
 
-      //PWM値に格納
+      //  PWM値に格納
       VEAB_desired[i] = veab_filter[i];
 
-      //前回のVEAB値に格納
+      //  前回のVEAB値に格納
       previous_value_veab[i] = veab_filter[i];
       
     }
@@ -262,27 +286,28 @@ void thread_callback() {
     int t1 = millis();
     //Serial.println(t1-t0);
 
-    //　制御周期を満たすように調整
+    //  制御周期を満たすように調整
     if( t1-t0 <= CONTROL_PERIOD_MS ){
-      // spare cpu slices to other threads for the remaining time
+      //  spare cpu slices to other threads for the remaining time
       threads.delay(CONTROL_PERIOD_MS - (t1-t0));
     } else {
-      // the designated period is violated
+      //  the designated period is violated
       error_loop();
     }
+
   }
 }
 
 //　Publish関数（ROS2）
 void timer_callback(rcl_timer_t * timer, int64_t last_call_time)
 {
-  //　引数last_call_timeは使わない
+  //  引数last_call_timeは使わない
   RCLC_UNUSED(last_call_time);
 
   if (timer != NULL) {
     //　Publlishするメッセージmsg_pubに格納
     for (size_t i = 0; i < ANALOG_IN_CH; i++) {
-      msg_pub.data.data[i] = ain[i];
+      msg_pub.data.data[i] = pub[i];
     }
     //　メッセージをトピックに送信
     RCSOFTCHECK(rcl_publish(&publisher, &msg_pub, NULL));
@@ -295,39 +320,43 @@ void subscription_callback(const void * msgin)
   //　受け取ったメッセージmsginをstd_msgs__msg__UInt16MultiArray型にキャスト
   const std_msgs__msg__UInt16MultiArray * msg = (const std_msgs__msg__UInt16MultiArray *)msgin;
   //  Subscribeしたメッセージを格納
-  for (size_t i = 0; i < ANALOG_OUT_CH; i++) {
-    aout[i] = msg->data.data[i];
+  for (size_t i = 0; i < SUBSCRIBE; i++) {
+    sub[i] = msg->data.data[i];
   }
+
+  //  Subscribe関数の初回判定
+  sub_count = 1;
   
 }
 
+//  PID制御関数
 void PID(){
   for(int i = 0; i < 6; i++){
 
-    //誤差計算
+    //  誤差計算
     errors[i] = POT_desired[i] - POT_realized[i];
 
-    //誤差の積分値計算
+    //  誤差の積分値計算
     integral[i] += errors[i];
 
-    //誤差の微分値計算
+    //  誤差の微分値計算
     de[i] = errors[i] - previous_errors[i];
 
-    //PID制御計算
+    //  PID制御計算
     outputPID[i] = (kp[i] * errors[i] + ki[i] * integral[i] + kd[i] * de[i]) * direction[i];
 
-    //VEAB1とVEAB2に与えるPWMの値を計算し格納
+    //  VEAB1とVEAB2に与えるPWMの値を計算し格納
     Result veab = calculate_veab_Values(outputPID[i], i);
     VEAB_desired[2*i] = veab.veab_value1;   //0, 2, 4, 6, 8, 10ピンへ
     VEAB_desired[2*i+1] = veab.veab_value2; //1, 3, 5, 7, 9, 11ピンへ
 
-    //計算に用いた誤差を前回の誤差に変更
+    //  計算に用いた誤差を前回の誤差に変更
     previous_errors[i] = errors[i];
 
   }
 }
 
-//VEAB1とVEAB2に与えるPWMの値の計算関数(Ballistic Modeにおける両ポートの値を基準に足し引きを行う)
+//  VEAB1とVEAB2に与えるPWMの値の計算関数(Ballistic Modeにおける両ポートの値を基準に足し引きを行う)
 Result calculate_veab_Values(float outputPID, int i) {
   Result result;
   if(i == 0){
@@ -358,15 +387,15 @@ Result calculate_veab_Values(float outputPID, int i) {
 
 //  ローパスフィルタ(RCフィルタ)関数
 int RC_LPF(int value, int previous_value, int initial_lpf, float coef_lpf){
-  //ローパスフィルタの値初期化
+  //  ローパスフィルタの値初期化
   int filter_value = 0;
 
-  //初回のみvalue値は同じものを使う
+  //  初回のみvalue値は同じものを使う
   if(initial_lpf == 0){
     previous_value = value;
   }
 
-  //ローパスフィルタ計算
+  //  ローパスフィルタ計算
   filter_value = previous_value * coef_lpf + value * (1 - coef_lpf);
 
   return filter_value;
@@ -376,21 +405,21 @@ int RC_LPF(int value, int previous_value, int initial_lpf, float coef_lpf){
 Result_LPF Moving_LPF() {
   Result_LPF lpf;
 
-  // 各チャンネルの処理
+  //  各チャンネルの処理
   for (int i = 0; i < NUM_CHANNELS; i++) {
-    int new_value = analogRead(ain_channels[i]); // 新しいアナログ値を読み取る
+    int new_value = analogRead(ain_channels[i]);  //  新しいアナログ値を読み取る
 
-    // リングバッファが満杯の場合、古い値を取り出して合計から引く
+    //  リングバッファが満杯の場合、古い値を取り出して合計から引く
     if (buffers[i].isFull()) {
-      pot_sum[i] -= buffers[i].first(); // 最古の値を引く
+      pot_sum[i] -= buffers[i].first(); //  最古の値を引く
     }
 
-    // 新しい値をバッファに追加し、合計を更新
+    //  新しい値をバッファに追加し、合計を更新
     buffers[i].push(new_value);
     pot_sum[i] += new_value;
 
-    // 移動平均を計算
-    POT[i] = pot_sum[i] / buffers[i].size(); // バッファ内の現在の要素数で割る
+    //  移動平均を計算
+    POT[i] = pot_sum[i] / buffers[i].size();  //  バッファ内の現在の要素数で割る
   }
 
   lpf.POT0 = POT[0];
@@ -432,8 +461,8 @@ void setup() {
   msg_pub.data.capacity = ANALOG_IN_CH; //  data配列の最大要素数=メッセージの要素数
   msg_pub.data.size = 0;  //　メッセージ送信時のデータの初期化の保証
   msg_pub.data.data = (uint16_t*)malloc(msg_pub.data.capacity * sizeof(uint16_t));  //　data配列に必要なメモリを動的に確保
-  msg_pub.layout.dim.capacity = 1; // 1-dimentional array: vector（次元の数）
-  msg_pub.layout.dim.size = 0;  //　配列の次元情報を初期化
+  msg_pub.layout.dim.capacity = 1;  //  1-dimentional array: vector（次元の数）
+  msg_pub.layout.dim.size = 0;  //  配列の次元情報を初期化
   msg_pub.layout.dim.data = (std_msgs__msg__MultiArrayDimension*) malloc(msg_pub.layout.dim.capacity * sizeof(std_msgs__msg__MultiArrayDimension)); //  MultiArrayDimension のためのメモリを確保
   for(size_t i = 0; i < msg_pub.layout.dim.capacity; i++){  //  ラベルフィールドの設定(コピペ)
     msg_pub.layout.dim.data[i].label.capacity = 20;
@@ -441,11 +470,11 @@ void setup() {
     msg_pub.layout.dim.data[i].label.data = (char*) malloc(msg_pub.layout.dim.data[i].label.capacity * sizeof(char));
   }
   
-  //　メッセージ変数msg_subに対して、メモリの確保と初期化
-  msg_sub.data.capacity = ANALOG_OUT_CH;  //  data配列の最大要素数=メッセージの要素数
+  //  メッセージ変数msg_subに対して、メモリの確保と初期化
+  msg_sub.data.capacity = SUBSCRIBE;  //  data配列の最大要素数=メッセージの要素数
   msg_sub.data.size = 0;  //　初期状態でメッセージを受け取ることを保証（初期化）
   msg_sub.data.data = (uint16_t*)malloc(msg_sub.data.capacity * sizeof(uint16_t));  //　data配列に必要なメモリを動的に確保
-  msg_sub.layout.dim.capacity = 1; // 1-dimentional array: vector
+  msg_sub.layout.dim.capacity = 1;  //  1-dimentional array: vector
   msg_sub.layout.dim.size = 0;  //　配列の次元情報を初期化
   msg_sub.layout.dim.data = (std_msgs__msg__MultiArrayDimension*) malloc(msg_sub.layout.dim.capacity * sizeof(std_msgs__msg__MultiArrayDimension)); //  MultiArrayDimension のためのメモリを確保
   for(size_t i = 0; i < msg_sub.layout.dim.capacity; i++){ //  ラベルフィールドの設定(コピペ)
@@ -463,12 +492,12 @@ void setup() {
   msg_pub.layout.dim.data[0].stride = ANALOG_IN_CH; //　publishするメッセージの更新ストライドを設定
   //  メッセージ変数を初期化
   for (size_t i = 0; i < ANALOG_IN_CH; i++) {
-    ain[i] = 0; //ain配列（アナログ入力の値）の初期化
-    msg_pub.data.data[i] = ain[i];  //  メッセージのデータ部分に初期化された値を格納
+    pub[i] = 0; //  pub配列（publishメッセージの値）の初期化
+    msg_pub.data.data[i] = pub[i];  //  メッセージのデータ部分に初期化された値を格納
   }
-  for (size_t i = 0; i < ANALOG_OUT_CH; i++) {
-    msg_sub.data.data[i] = 255; //アナログ出力データを初期化
-    aout[i] = msg_sub.data.data[i]; //  アナログ出力データをaout配列にコピー
+  for (size_t i = 0; i < SUBSCRIBE; i++) {
+    msg_sub.data.data[i] = 255; //  sub配列（subscribeメッセージの値）の初期化
+    sub[i] = msg_sub.data.data[i]; //  メッセージデータをsub配列にコピー
     
   }
 
@@ -478,13 +507,13 @@ void setup() {
   // micro-ROS functions setup
   //****************************************
 
-  // ①create default allocator（メモリ管理のためのアロケータ（メモリ確保用の管理機構）を取得）
+  //  ①create default allocator（メモリ管理のためのアロケータ（メモリ確保用の管理機構）を取得）
   allocator = rcl_get_default_allocator();
 
-  // ②create init_options（サポートオプションを設定。引数もコピペでよい）
+  //  ②create init_options（サポートオプションを設定。引数もコピペでよい）
   RCCHECK(rclc_support_init(&support, 0, NULL, &allocator));
 
-  // ③create node（引数：(初期化するノード, ノードの名前, ノード空間の名前, サポート構造体)）
+  //  ③create node（引数：(初期化するノード, ノードの名前, ノード空間の名前, サポート構造体)）
   RCCHECK(rclc_node_init_default(&node, "micro_ros_arduino_node", "", &support));
 
   //------create subscriber（subscriberの初期化）--------------------------------------------------------------------
@@ -539,10 +568,12 @@ void setup() {
   analogWrite(aout_channels[11], 124);
 
   //  移動平均法1回目の処理(ポテンショメータ)
-  for (int i = 0; i < LPF_kosuu; i++){
+  for (int i = 0; i < LPF_KOSUU; i++){
     Moving_LPF();
   }
 
+
+  delay(5000);
   //==========================================================
 
   // turn off LED
@@ -555,7 +586,7 @@ void setup() {
 
 void loop() {
 
-  // spin the ros tasks（Executorを実行）
+  //  spin the ros tasks（Executorを実行）
   RCSOFTCHECK(rclc_executor_spin_some(&executor, RCL_MS_TO_NS(10)));
 
 }

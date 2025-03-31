96c61fa7a194c3083e91c1761aa200986c74f836
Nihiro Tamura <tamura.nihiro@irl.sys.es.osaka-u.ac.jp>
2025-03-31 18:50:02 +0900

    board2用(LAN接続)※ゲインチューニング未完No3

--- ADRC_ROS2-board2/ADRC_ROS2-board2.ino
+++ ADRC_ROS2-board2/ADRC_ROS2-board2.ino
@@ -391,7 +391,7 @@ void thread_callback() {
     }
 
     //  シリアルモニタに表示
-    /**/
+    /*
     Serial.print(POT_realized[0]);
     Serial.print(",");
     Serial.print(z1[0]);
@@ -410,13 +410,13 @@ void thread_callback() {
     Serial.print(",");
     Serial.print(outputADRC[0]);
     Serial.print(",");
-    Serial.println(POT_desired[0]);
+    Serial.println(POT_desired[0]);*/
 
 
     //------VEABへ出力--------------------------------------------------------------------
-    /*ピン0,1*/
+    /*ピン0,1
     analogWrite(aout_channels[0], VEAB_desired[0]);
-    analogWrite(aout_channels[1], VEAB_desired[1]);
+    analogWrite(aout_channels[1], VEAB_desired[1]);*/
     /*ピン2,3
     analogWrite(aout_channels[2], VEAB_desired[2]);
     analogWrite(aout_channels[3], VEAB_desired[3]);*/
